d2dd30772247e07c9602368ca5b18e50a52a30f0
Azechum30 <azechum21@gmail.com>
2024-09-19 22:13:55 +0000

    Added git a CI\/CD pipeline with github actions

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage ()
     return (
         <>
             <div className="bg-gray-100 h-16 border-b mb-6 -mt-4 lg:-mr-6  p-4">
-                <h1 className="text-xl font-bold">Statistics</h1>
+                <h1 className="text-xl font-bold">Metrics</h1>
             </div>
             <Statistics />
             <NewlyAddedMembers />
